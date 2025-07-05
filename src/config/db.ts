216bfa727d52140f787c42d229962247f1914451
Rohit Yadav <rk4740779@gmail.com>
2025-07-05 14:23:27 +0530

    db fix

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async (): Promise<void> => {
   try {
-    const mongoUri = process.env.MONGO_URI;
+    const mongoUri = process.env.MONGO_URI!;
     
     await mongoose.connect(mongoUri);
     
